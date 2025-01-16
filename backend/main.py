62b1e492e650e26d6a3c61fbbd68e50b8b572678
seoraroong <karen0705@naver.com>
2025-01-16 20:56:00 +0900

    🔨Refactor: user 라우터 추가

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from backend.app.routers import users  # users 모듈을 가져오기
 
 app = FastAPI()
+
+app.include_router(users.router, prefix="/api")
 
 @app.get("/")
 def read_root():
